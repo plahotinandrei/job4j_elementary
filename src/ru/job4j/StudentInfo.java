d09f50c7a49baf903f21e24baef0c23cd7a0d6ee
Andrey Plakhotin <plahotin94@yandex.ru>
2021-04-19 23:02:25 +0300

    3.3. Разбор первой программы. [#521418]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Andrey Plahotin");
+        System.out.println("06.07.1994");
+    }
+}
