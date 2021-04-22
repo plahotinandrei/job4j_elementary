64698ae4f56d467c6af84cae9a05e4327d0842c1
Andrey Plakhotin <plahotin94@yandex.ru>
2021-04-23 00:16:59 +0300

    4.2. Тернарное сравнение [#521358]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int max = Max.max(1, 2);
+        System.out.println("Максимальное из 1 и 2 это " + max);
+    }
+}
