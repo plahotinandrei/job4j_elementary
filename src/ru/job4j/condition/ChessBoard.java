fcd2701a72f89091a3e7bcf9fe59efe866493023
Andrey Plakhotin <plahotin94@yandex.ru>
2021-04-24 00:13:45 +0300

    4.3.6. Math.abs [#521357]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int rsl = 0;
+        boolean condition = Math.abs(x2 - x1) == Math.abs(y2 - y1);
+        boolean startInRange =  x1 >= 0 && x1 <= 7 && y1 >= 0 && y1 <= 7;
+        boolean finishInRange =  x2 >= 0 && x2 <= 7 && y2 >= 0 && y2 <= 7;
+        if(condition && startInRange && finishInRange) {
+            rsl = Math.abs(x2 - x1);
+            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+        }
+
+        return rsl;
+    }
+}
