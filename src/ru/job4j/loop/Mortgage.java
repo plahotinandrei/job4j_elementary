0bf8f4c5ca49352e563f746e546398947e9a69e8
Andrey Plakhotin <plahotin94@yandex.ru>
2021-04-24 23:56:34 +0300

    5.7. Ипотека [#521458]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 1;
+        double balance = amount;
+
+        while(balance + balance * percent * 0.01 - salary > 0) {
+            balance = balance + balance * percent * 0.01 - salary;
+            year++;
+        }
+
+        return year;
+    }
+}
