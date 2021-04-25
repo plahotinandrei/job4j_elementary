7e9ef06a56bcf141c5fff6ca281170454fef5114
Andrey Plakhotin <plahotin94@yandex.ru>
2021-04-25 21:36:40 +0300

    5.7. Ипотека [#521458] Fix

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -1,12 +1,11 @@
 package ru.job4j.loop;
 
 public class Mortgage {
-    public static int year(int amount, int salary, double percent) {
-        int year = 1;
-        double balance = amount;
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
 
-        while (balance + balance * percent * 0.01 - salary > 0) {
-            balance = balance + balance * percent * 0.01 - salary;
+        while (amount > 0) {
+            amount = amount + amount * percent * 0.01 - salary;
             year++;
         }
 
