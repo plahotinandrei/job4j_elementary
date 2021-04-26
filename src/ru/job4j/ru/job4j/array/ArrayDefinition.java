fb180113d3cff569d6b061eb01a851d858b28824
Andrey Plakhotin <plahotin94@yandex.ru>
2021-04-26 23:38:24 +0300

    6.0.2. Заполнение массива. [#521379]

--- src/ru/job4j/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,16 @@ public class ArrayDefinition {
 
         float[] prices = new float[40];
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Andrey Plahotin";
+        names[1] = "Petr Arsentev";
+        names[2] = "Ivan Ivanov";
+        names[3] = "Sergey Petrov";
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
